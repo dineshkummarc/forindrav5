30be68b2fa9a0fbccc25af82dd56a9cb45d49741
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2020-11-14 15:27:56 +0530

    update

--- application/views/admin/v_dashboard.php
+++ application/views/admin/v_dashboard.php
@@ -11,7 +11,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Forindra | Dashboard</title>
+  <title>Forum | Dashboard</title>
   <!-- Tell the browser to be responsive to screen width -->
   <link rel="shorcut icon" type="text/css" href="<?php echo base_url().'theme/images/icon2-01.png'?>">
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
