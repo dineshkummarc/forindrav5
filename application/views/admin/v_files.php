e87135c2497d463c6d2702811753c54021973c2b
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2020-11-14 15:27:32 +0530

    update

--- application/views/admin/v_files.php
+++ application/views/admin/v_files.php
@@ -10,7 +10,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Forindra | Files</title>
+  <title>Forum | Files</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <link rel="shorcut icon" type="text/css" href="<?php echo base_url().'theme/images/icon2-01.png'?>">
