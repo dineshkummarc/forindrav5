1f265eaa314e26a2a679ef374ee46017ddfd4778
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2020-11-14 15:03:14 +0530

    update

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 // $config['base_url'] = 'http://localhost/forindrav5/';
-$config['base_url'] = 'http://local.rocket.co.in:8081/forum.test/forindrav5/';
+$config['base_url'] = 'http://local.rocket.co.in:8081/CheckThesePortals/forindrav5/';
 
 /*
 |--------------------------------------------------------------------------
